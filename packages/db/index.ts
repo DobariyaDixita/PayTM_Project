2870725761db8e3d6f7aad439320b804146d046f
DobariyaDixita <dixitadobariya100@gmail.com>
2024-08-19 15:36:20 -0400

    added package-lock.json file

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,16 +1,100 @@
-import { PrismaClient } from "@prisma/client";
+import { currentUser } from "@clerk/nextjs/server";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+import prisma from "@repo/db/client";
+import CardInfo from "../../../components/CardInfo";
+import { getAllBudget } from "../../lib/actions/budget";
+import { BudgetItem } from "../../../components/BudgetItem";
+import BarGraph from "../../../components/BarGraph";
+import Link from "next/link";
 
-declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>;
+
+export default async function Page() {
+  const user = await currentUser();
+  const userEmail = user?.primaryEmailAddress?.emailAddress;
+
+  const data = await getAllBudget(userEmail!);
+
+  const budgetListWithExpenses = await prisma.budget.findMany({
+    where: {
+      createdBy: userEmail,
+    },
+    include: {
+      expenses: true,
+    },
+  });
+
+  const budgetData = budgetListWithExpenses.map((budget) => {
+    const expenseAmount = budget.expenses.reduce(
+      (sum, expense) => sum + expense.expenseAmount,
+      0
+    );
+    return {
+      budgetName: budget.name,
+      budgetAmount: budget.amount,
+      expenseAmount: expenseAmount,
+    };
+  });
+
+  const totalBudgetAmount = await prisma.budget.aggregate({
+    where: {
+      createdBy: userEmail,
+    },
+    _sum: {
+      amount: true,
+    },
+    _count: {
+      id: true,
+    },
+  });
+
+  const expenses = await prisma.budget.findMany({
+    where: {
+      createdBy: userEmail,
+    },
+    include: {
+      expenses: {
+        select: {
+          expenseAmount: true,
+        },
+      },
+    },
+  });
+
+  const expensesInfo = expenses.flatMap((e) => e.expenses);
+  const expenseAmount = expensesInfo.reduce((prev, curr) => {
+    return prev + curr.expenseAmount;
+  }, 0);
+
+  return (
+    <div className="p-8">
+      <div className="text-2xl font-bold">Hi , {user?.fullName}</div>
+      <div className="text-base text-gray-700 opacity-70">
+        Manage your expenses from dashboard
+      </div>
+      <div className="mt-5">
+        <CardInfo
+          totalBudget={totalBudgetAmount._sum.amount}
+          totalExpense={expenseAmount}
+          noOfBudgets={totalBudgetAmount._count.id}
+        />
+      </div>
+      <div className="mt-10 grid md:grid-cols-12">
+        <div className="col-span-8">
+          {budgetData && <BarGraph budgetData={budgetData} />}
+        </div>
+        <div className="px-5 col-span-4">
+          <h2 className="font-bold text-xl">Latest Budgets</h2>
+          <div className="grid gap-2 mt-2">
+            {data.map((budget) => {
+              return (
+                <Link href={`/expense/${budget.id}`} key={budget.id}>
+                  <BudgetItem budget={budget} />
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-const prisma: ReturnType<typeof prismaClientSingleton> =
-  globalThis.prismaGlobal ?? prismaClientSingleton();
-
-export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
